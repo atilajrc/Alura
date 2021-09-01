5a24eb0c9b3b1f14d80b6e76e3788f1e5045dbc4
atilajrc <atilajrc@gmail.com>
2021-09-01 10:58:54 -0300

    JavaSript

--- Java Script/Orientacao Objetos/index.js
+++ Java Script/Orientacao Objetos/index.js
@@ -5,13 +5,13 @@ const cliente1 = new Cliente("Ricardo", 11122233309);
 const cliente2 = new Cliente("Alice", 88822233309);
 
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
+const conta1 = new ContaCorrente(1001, cliente1);
+conta1.depositar(1000);
+conta1.sacar(100);
 
-const conta2 = new ContaCorrente(102, cliente2);
-
+const conta2 = new ContaCorrente(1002, cliente2);
 let valor = 200;
-contaCorrenteRicardo.tranferir(valor, conta2);
+conta1.tranferir(valor, conta2);
 
-console.log(contaCorrenteRicardo);
+console.log(conta1);
+console.log(conta2);
